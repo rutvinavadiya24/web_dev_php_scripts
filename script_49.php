d12bb98184fa8c7f3bbd4450c0386590a1b28683
rutvinavadiya24 <143237304+rutvinavadiya24@users.noreply.github.com>
2023-10-27 21:52:08 +0530

    Delete script_49.php

--- script_49.php
+++
@@ -1,9 +0,0 @@
-<?php
-$flowers=array("rose","sunflower","lily","merigold","lotus","jasmin");
-print_r(array_chunk($flowers,2));
-
-/*\
-output:
-Array ( [0] => Array ( [0] => rose [1] => sunflower ) [1] => Array ( [0] => lily [1] => merigold ) [2] => Array ( [0] => lotus [1] => jasmin ) )
-*/
-?>
