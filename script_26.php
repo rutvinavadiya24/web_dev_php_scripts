223acaf2bf5f0b5138bbcfd297df714e8c92e9fd
rutvinavadiya24 <143237304+rutvinavadiya24@users.noreply.github.com>
2023-10-27 21:48:40 +0530

    Delete script_26.php

--- script_26.php
+++
@@ -1,9 +0,0 @@
-<?php
-function generateRandomNumber($min, $max) {
-   return rand($min, $max);
-}
-$minimum = 1;
-$maximum = 100;
-$randomNumber = generateRandomNumber($minimum, $maximum);
-echo "Random number between $minimum and $maximum: $randomNumber";
-?>
